42ba3a58a2c496dac9b2f1bf4ffe8cb4a979f4a6
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-10-22 12:39:12 +0630

    Add project files.

---
+++ FlowbiteDotNetMvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlowbiteDotNetMvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
